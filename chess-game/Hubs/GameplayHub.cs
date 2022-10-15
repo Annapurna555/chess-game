96c4e2e95ad1efe73fe15472edd891a22aa7a28b
Pawel K <pawelkarabowicz@gmail.com>
2022-10-15 15:04:41 +0200

    feat: Add group to enable run multiple games at the same time

--- chess-game/Hubs/GameplayHub.cs
+++ chess-game/Hubs/GameplayHub.cs
@@ -4,9 +4,34 @@ namespace chess_game.Hubs;
 
 public class GameplayHub : Hub
 {
-    public async Task MakeMoveOnTheBoard(string pieceId, string fieldId)
+    private static List<string> _connectionsId = new List<string>();
+    public async Task MakeMoveOnTheBoard(string pieceId, string fieldId, string gameplayId)
     {
-        await Clients.All.SendAsync("movePiece", pieceId, fieldId);
+        await Clients.OthersInGroup(gameplayId).SendAsync("movePiece", pieceId, fieldId);
+        
+    }
+    
+    public override async Task OnConnectedAsync()
+    {
+        _connectionsId.Add(Context.ConnectionId);
+        if (_connectionsId.Count >= 2)
+        {
+            await CreateGroup(); 
+        }
+        else
+        {
+            Console.WriteLine("Not sufficient users connected to create group ");  //for tests purposes
+           
+        }
+    }
+
+    public async Task CreateGroup()
+    {
+        var groupname = _connectionsId[0] + _connectionsId[1]; 
+        await Groups.AddToGroupAsync(_connectionsId[0], groupname);
+        await Groups.AddToGroupAsync(_connectionsId[1], groupname);
+        _connectionsId.RemoveRange(0, 2);
+        await Clients.Group(groupname).SendAsync("CreateGameplayId", groupname);
     }
     
 }
